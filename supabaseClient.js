e9bf52a872b247d275022e3b834e6fbd70b473c5
eyosk <93485934+eyosk@users.noreply.github.com>
2024-12-17 13:07:12 -0500

    Delete supabaseClient.js
    There is now to supabaseClient.js files, on being daily_exchange_supabaseClient.js and the other 
    being historical_supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-const supabaseUrl = 'https://pjlouvsxymvfdmkdyupy.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqbG91dnN4eW12ZmRta2R5dXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzMDAzMjQsImV4cCI6MjA0OTg3NjMyNH0.JPZZqEQPB5KHUv81D9sLH08ikjDCw3ftl_hEJG1VSKA';
-
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
